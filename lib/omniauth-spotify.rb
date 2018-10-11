77ec0bf5c8cf7625223f7b00953acb23a9bbfd6d
Vladislav Kovalskij <vladislav.kovalskij@gmail.com>
2018-10-11 16:14:08 +0300

    Update omniauth-spotify.rb

--- lib/omniauth-spotify.rb
+++ lib/omniauth-spotify.rb
@@ -83,7 +83,7 @@ module OmniAuth
           ''
         else
           puts full_host + script_name + callback_path 
-          full_host + script_name + callback_path
+          (full_host + script_name + callback_path)
         end
       end
     end
